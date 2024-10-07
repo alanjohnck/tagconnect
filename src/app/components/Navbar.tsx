c0193ff8287d994efe9dbca5498182da2d9dc6f8
alanjohnck <alanjohnchacko.live@gmail.com>
2024-10-07 22:41:30 +0530

    contact

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,8 +6,8 @@ function Navbar() {
 
   return (
     <nav className='w-[98%] h-[8vh] flex justify-start items-center  bg-black  text-white rounded-lg shadow-sm border-1'>
-       <div className='w-[10%]  flex'>
-          <div className='w-10 h-6 flex justify-center items-center px-2'>
+       <div className='w-[10%] h-fit flex px-4'>
+          <div className='w-8 md:w-8 lg:w-6 h-6 flex justify-center items-center '>
             <Link href="/landing"><img src='./LogoWhiteNonTransperent.png' className='w-fit object-fit'></img></Link>
           </div>  
        </div>
--- src/app/landing/sections/Hero.tsx
+++ src/app/landing/sections/Hero.tsx
@@ -5,11 +5,11 @@ import React from 'react';
 
 function Hero() {
   return (
-    <div className='w-screen h-screen flex flex-col items-center justify-center bg-white'>
+    <div className='w-screen h-fit flex flex-col items-center justify-center bg-white'>
       <div className='p-2 h-[40] w-full flex items-center justify-center sticky top-5'>
         <Navbar />
       </div>
-      <div className='h-[40rem]'>
+      <div className='h-screen'>
        <TextHoverEffect text='tagdynamix' />
       </div>
     </div>
